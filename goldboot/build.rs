d8b970bb47a7f4549aaaf629b40d5c85c3aaca89
Tyler Cook <10459406+cilki@users.noreply.github.com>
2024-03-17 15:31:47 -0500

    chore: release

--- goldboot/build.rs
+++ goldboot/build.rs
@@ -1,11 +1,10 @@
-use std::{
-    env,
-    path::{Path, PathBuf},
-};
+use std::{env, path::PathBuf};
 
 fn main() {
-    let dest =
-        std::path::Path::new(&env::var("OUT_DIR").expect("OUT_DIR not set")).join("built.rs");
-    built::write_built_file_with_opts(Some(&PathBuf::from("..")), &dest)
-        .expect("Failed to acquire build-time information");
+    if built::write_built_file().is_err() {
+        let dest =
+            std::path::Path::new(&env::var("OUT_DIR").expect("OUT_DIR not set")).join("built.rs");
+        built::write_built_file_with_opts(Some(&PathBuf::from("..")), &dest)
+            .expect("Failed to acquire build-time information");
+    }
 }
--- goldboot/src/registry/extract.rs
+++ goldboot/src/registry/extract.rs
@@ -21,21 +21,6 @@ impl FromRequest<RegistryState> for ImageHandle {
     type Rejection = StatusCode;
 
     async fn from_request(req: Request, state: &RegistryState) -> Result<Self, Self::Rejection> {
-        let (mut parts, body) = req.into_parts();
-
-        // We can use other extractors to provide better rejection messages.
-        // For example, here we are using `axum::extract::MatchedPath` to
-        // provide a better error message.
-        //
-        // Have to run that first since `Json` extraction consumes the request.
-        let path = parts
-            .extract::<MatchedPath>()
-            .await
-            .map(|path| path.as_str().to_owned())
-            .ok();
-
-        let req = Request::from_parts(parts, body);
-
         match Path::<HashMap<String, String>>::from_request(req, state).await {
             Ok(value) => match value.get("image_id") {
                 Some(image_id) => match ImageLibrary::find_by_id(image_id) {
